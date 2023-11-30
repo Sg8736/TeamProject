2fcca467b71e8f94b3ffe0a353aece63c83b5d7a
DESKTOP-LV2GEF9\minuk <kimminuk0125@gmail.com>
2023-11-30 15:53:31 +0900

    2023-11-30 16:53 map 기능 추가

---
+++ 팀플 (2023-11-23)/TeamProject/TeamProject/KakaoApi.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace TeamProject
+{
+    class KakaoApi
+    {
+        public static List<Locale> Search(string text)
+        {
+            List<Locale> list = new List<Locale>();
+
+            string url = "https://dapi.kakao.com/v2/local/search/keyword.json";
+            string query = $"{url}?query={text}";
+            string restAPIKey = "53921425e8a905e1f17c4f92a05dcaf6";
+            string Header = $"KakaoAK {restAPIKey}";
+            WebRequest request = WebRequest.Create(query);
+            request.Headers.Add("Authorization", Header);
+
+            //응답받기
+            WebResponse response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+            string json = reader.ReadToEnd();
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+
+            //dynamic
+            dynamic dob = js.Deserialize<dynamic>(json);
+            dynamic docs = dob["documents"];
+            object[] buf = docs;
+            int length = buf.Length;
+            for (int i=0; i<length; i++)
+            {
+                string lname = docs[i]["place_name"];
+                double x = double.Parse(docs[i]["x"]);
+                double y = double.Parse(docs[i]["y"]);
+                list.Add(new Locale(lname, y, x));
+            }
+            return list;
+        }
+    }
+}
---
+++ 팀플 (2023-11-23)/TeamProject/TeamProject/map.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeamProject
+{
+    public partial class map : Form
+    {
+        public map()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                //엔터를 누르면 버튼을 누른다는 뜻
+                button1.PerformClick();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Locale> locales = KakaoApi.Search(textBox1.Text);
+            listBox1.Items.Clear();
+            foreach (Locale locale in locales)
+            {
+                listBox1.Items.Add(locale);
+            }
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+                return;
+            Locale ml = listBox1.SelectedItem as Locale;
+            object[] pos = new object[] { ml.Lat, ml.Lng };
+            HtmlDocument hdoc = webBrowser1.Document;
+            hdoc.InvokeScript("setCenter", pos);
+        }
+    }
+}
